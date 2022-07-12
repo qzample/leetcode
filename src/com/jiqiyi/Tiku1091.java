b32645fa02351307b7454be057f4703c0200a1d5
朱肖龙 <xlong_zhu@foxmail.com>
2022-07-13 07:21:26 +0800

    20220712 Signed-off-by: <xlong_zhu@foxmail.com>

--- src/com/jiqiyi/Tiku1091.java
+++ src/com/jiqiyi/Tiku1091.java
@@ -43,22 +43,28 @@ class Tiku1091 {
         int[][] dict = new int[][]{{1,0},{-1,0},{0,1},{0,-1},{1,-1},{1,1},{-1,1},{-1,-1}};
         int m = grid.length;
         int n = grid[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==0){
-
+        if(grid[0][0]!=0 || grid[m-1][n-1]!=0) return -1;
+        boolean[][] isVisited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0,0});
+        int path = 0;
+        while(!queue.isEmpty()){
+            path++;
+            for(int size = queue.size()-1;size>=0;size--){
+                int[] pos = queue.poll();
+                int x = pos[0];
+                int y = pos[1];
+                if(x==m-1 && y==n-1) return path;
+                for(int i=dict.length-1;i>=0;i--){
+                    int dx = x+dict[i][0];
+                    int dy = y+dict[i][1];
+                    if(dx<0 || dx>=m || dy<0 || dy>=n) continue;
+                    if(isVisited[dx][dy] || grid[x][y]==1) continue;
+                    isVisited[dx][dy] = true;
+                    queue.offer(new int[]{dx,dy});
                 }
             }
         }
         return -1;
-    }
-
-    boolean[][] visited = new boolean[][];
-
-    public void dfs(int[][] grid, int x, int y, int cnt){
-        if(x<0 || x>=grid.length || y<0 || y>=grid[0].length || grid[x][y]>0) return;
-        grid[x][y] -= 1;
-        cnt++;
-        if(x==grid.length-1 && y==grid[0].length-1) return;
     }
 }
