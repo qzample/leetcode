7c1d76b2bbf6bc2f2b6a158feb9a8a561bc95104
朱肖龙 <xlong_zhu@foxmail.com>
2022-07-19 22:36:55 +0800

    20220719

--- src/com/jiqiyi/Tiku45.java
+++ src/com/jiqiyi/Tiku45.java
@@ -1,7 +1,25 @@
 package com.jiqiyi;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Tiku45 {
-    // public int jump(int[] nums) {
-        
-    // }
+    public int jump(int[] nums) {
+        if(nums.length==1) return 0;
+        int n = nums.length;
+        int start = 0;
+        int end = 0;
+        int step = 0;
+        while(end<n){
+            step++;
+            int furthest = 0;
+            for(int i=start;i<=end;i++){
+                furthest = Math.max(furthest, i+nums[i]);
+            }
+            if(furthest>=n-1) break;
+            start = end + 1;
+            end = furthest;
+        }
+        return step;
+    }
 }
